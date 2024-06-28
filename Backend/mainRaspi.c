28178e993f36b19cd230c0f30350b8ac420175a4
MRubacha5 <mrubacha@itba.edu.ar>
2024-06-28 17:27:51 -0300

    raspi

--- Backend/mainRaspi.c
+++ Backend/mainRaspi.c
@@ -6,48 +6,36 @@ Permite mover con el joystick un LED encendido en la matriz de LEDs.
 #include <stdio.h>
 #include "disdrv.h"
 #include "joydrv.h"
+#include "Objetos.h"
+#include "Rana.h"
 
 #define THRESHOLD 40	//Límite a partir del cual se mueve el LED encendido
 
 int main(void)
 {
+    linea_t * pl = CreateWorld(16, 10);
+    rana_t rana = {.posx=10/2, .posy=0, .vidas=3};
 	
 	joy_init();										//inicializa el joystick
 	disp_init();									//inicializa el display
 	disp_clear();									//limpia todo el display
 	dcoord_t pos = {DISP_MAX_X>>1 , DISP_MAX_Y>>1};	//pos es la posición actual, empieza en el centro de la matriz
-	dcoord_t npos = pos;							//npos es la próxima posición
 	joyinfo_t coord = {0,0,J_NOPRESS};							//coordenadas medidas del joystick
 	do
 	{
-		printf("Joystick: (%4d,%4d)" , coord.x, coord.y);	//Imprime las coordenadas del joystick
-		printf(" | ");
-		printf("Display: (%2d,%2d)\n" , npos.x, npos.y);	//Imprime la posición del LED encendio
-		
 		disp_update();	//Actualiza el display con el contenido del buffer
 		coord = joy_read();	//Guarda las coordenadas medidas
-		
-		//Establece la próxima posición según las coordenadas medidas
-		if(coord.x > THRESHOLD && npos.x < DISP_MAX_X)
-		{
-			npos.x++;
-		}
-		if(coord.x < -THRESHOLD && npos.x > DISP_MIN)
-		{
-			npos.x--;
-		}
-		if(coord.y > THRESHOLD && npos.y > DISP_MIN)
-		{
-			npos.y--;
-		}
-		if(coord.y < -THRESHOLD && npos.y < DISP_MAX_Y)
-		{
-			npos.y++;
-		}
-		
-		disp_write(pos,D_OFF);	//apaga la posición vieja en el buffer
-		disp_write(npos,D_ON);	//enciende la posición nueva en el buffer
-		pos = npos;				//actualiza la posición actual
+		disp_clear();
+        int i, c;
+		for(i = 0 ; i < 16 ; i++){
+            linea_t * linea = pl+i;
+            for(c = 0 ; c < 10 ; c++){
+                pos.x = c;
+                pos.y = i;
+                disp_write(pos, !(linea->val_def));
+            }
+        }
+
 		
 	} while( coord.sw == J_NOPRESS );	//termina si se presiona el switch
 	
