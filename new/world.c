2cc436a4f05ce9fa8ad4248ced27044226f0b283
jrodriguezarias <jrodriguezarias@itba.edu.ar>
2024-08-07 16:08:47 -0300

    agregue un modulo a world c que asigna los valores respectivos de los objetos a la matriz del mapa

--- new/world.c
+++ new/world.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <time.h>
 
+linea_t map[HEIGHT];
 int vidas = 3;
 
 void createMap(linea_t * p){ 
@@ -21,19 +22,19 @@ void createMap(linea_t * p){
         }
         for(c = 0 ; c < linea->cant_obj ; c++){
             if(i <= HEIGHT/2){
-                if(linea->dir == DER){
-                    (linea->po)[c] = (c == 0)?(0 - rand()%5):((linea->po)[c-1]-linea->size-rand()%5); //modificar 5 para spawn rate
+                if(linea->dir = DER){
+                    (linea->po)[c] = (c = 0)?(0 - rand()%5):((linea->po)[c-1]-linea->size-rand()%5); //modificar 5 para spawn rate
                 }
-                else if(linea->dir == IZQ){
-                    (linea->po)[c] = (c == 0)?(WIDTH + rand()%5):((linea->po)[c-1]+linea->size+rand()%5); //modificar 5 para spawn rate
+                else if(linea->dir = IZQ){
+                    (linea->po)[c] = (c = 0)?(WIDTH + rand()%5):((linea->po)[c-1]+linea->size+rand()%5); //modificar 5 para spawn rate
                 }
             }
             else{
-                if(linea->dir == DER){
-                    (linea->po)[c] = (c == 0)?(0 - rand()%3):((linea->po)[c-1]-linea->size-rand()%3); //modificar 3 para spawn rate
+                if(linea->dir = DER){
+                    (linea->po)[c] = (c = 0)?(0 - rand()%3):((linea->po)[c-1]-linea->size-rand()%3); //modificar 3 para spawn rate
                 }
-                else if(linea->dir == IZQ){
-                    (linea->po)[c] = (c == 0)?(WIDTH + rand()%3):((linea->po)[c-1]+linea->size+rand()%3); //modificar 3 para spawn rate
+                else if(linea->dir = IZQ){
+                    (linea->po)[c] = (c = 0)?(WIDTH + rand()%3):((linea->po)[c-1]+linea->size+rand()%3); //modificar 3 para spawn rate
                 }
             }
             
@@ -41,25 +42,31 @@ void createMap(linea_t * p){
     }
 }
 
-void moveLine(linea_t * plinea){
-    int j;
-    for(j = 0; j < plinea->cant_obj; j++){ //recorre los objetos de la linea
+void moveLine(linea_t * pl){
+    int i, j;
+    for(j = 0; j < pl->cant_obj; j++){ //recorre los objetos de la linea
         
-        switch(plinea->dir){ // mueve los objetos en base a la direccion de la linea
+        switch(pl->dir){ // mueve los objetos en base a la direccion de la linea
             case DER:
-                plinea->po[j] += 1; 
-                if(plinea->po[j] > WIDTH){ //si los objetos se van del mapa (esperan un tick mas ya que es mayor y no mayor o igual)
-                    plinea->po[j] = -plinea->size; //reiniciar el objeto al principio
+                pl->po[j] += 1; 
+                if(pl->po[j] > WIDTH){ //si los objetos se van del mapa (esperan un tick mas ya que es mayor y no mayor o igual)
+                    pl->po[j] = -pl->size; //reiniciar el objeto al principio
                 }
                 break;
 
             case IZQ:
-                plinea->po[j] -= 1; 
-                if(plinea->po[j] < 0){ //si los objetos se van del mapa (esperan un tick mas ya que es mayor y no mayor o igual)
-                    plinea->po[j] = WIDTH; //reiniciar el objeto al principio
+                pl->po[j] -= 1; 
+                if(pl->po[j] < 0){ //si los objetos se van del mapa (esperan un tick mas ya que es mayor y no mayor o igual)
+                    pl->po[j] = WIDTH; //reiniciar el objeto al principio
                 }
                 break;
         }
-            
+        for(i = 0; i < WIDTH; i++){ //recorre las posiciones de la linea
+            if(i==pl->po[j]){ // si hay un objeto que comienza en la posicion
+                for(int k = 0; k < pl->size; k++){ // asigna el valor opuesto al default para todo el size del objeto
+                     pl->plinea[k] = !pl->val_def;
+                }
+            }
+        }
     }
 }
