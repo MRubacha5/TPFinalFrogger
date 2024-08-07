ff6afa7601c68b7351ef2712f8c4b3c05c9657cf
MRubacha5 <mrubacha@itba.edu.ar>
2024-08-07 10:12:00 -0300

    borramos objetos punto h

--- new/Objetos.h
+++ new/Objetos.h
@@ -19,7 +19,7 @@ typedef struct{
     uint8_t val_def; //valor default de la linea. 0 es agua y 1 es tierra
     uint8_t cant_obj; //cantidad maxima de objetos que hay cargada en un momento 
     int * p_linea; //puntero a coordenada (0, linea)
-    objeto_t * po; //puntero a objetos
+    int * po; //arreglo que guarda posicion x de los objetos en la linea
 } linea_t;
 
 linea_t * CreateWorld(unsigned int h, unsigned int w);
