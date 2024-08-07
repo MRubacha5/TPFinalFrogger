a867ade15d99451f79d1e7938c5d445236f8954e
jrodriguezarias <jrodriguezarias@itba.edu.ar>
2024-08-07 11:49:42 -0300

    funcion moveWorld

--- old/world.c
+++ old/world.c
@@ -11,7 +11,7 @@ void createMap(linea_t * p){
 
     for(i = 0 ; i < HEIGHT ; i++){
         linea_t * linea = p + i;
-        linea->cant_obj = (i==0||i==HEIGHT/2||i==HEIGHT-1)?0:3;
+        linea->cant_obj = 3;
         linea->dir = (rand()%2?DER:IZQ);
         linea->size = 3;
         linea->v = 1;
@@ -19,25 +19,6 @@ void createMap(linea_t * p){
 
         for(c = 0 ; c < WIDTH ; c++){
             (linea->plinea)[c] = linea->val_def;
-        }
-        for(c = 0 ; c < linea->cant_obj ; c++){
-            if(i <= HEIGHT/2){
-                if(linea->dir = DER){
-                    (linea->po)[c] = (c = 0)?(0 - rand()%5):((linea->po)[c-1]-rand()%5); //modificar 5 para spawn rate
-                }
-                else if(linea->dir = IZQ){
-                    (linea->po)[c] = (c = 0)?(WIDTH + rand()%5):((linea->po)[c-1]+rand()%5); //modificar 5 para spawn rate
-                }
-            }
-            else{
-                if(linea->dir = DER){
-                    (linea->po)[c] = (c = 0)?(0 - rand()%3):((linea->po)[c-1]-rand()%3); //modificar 3 para spawn rate
-                }
-                else if(linea->dir = IZQ){
-                    (linea->po)[c] = (c = 0)?(WIDTH + rand()%3):((linea->po)[c-1]+rand()%3); //modificar 3 para spawn rate
-                }
-            }
-            
         }
     }
 }
