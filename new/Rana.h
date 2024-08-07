5c04b375d66b334b6b3e686dd9391872b359ba88
jrodriguezarias <jrodriguezarias@itba.edu.ar>
2024-08-07 11:03:58 -0300

    algunos cambios a rana como para poder utilizarlo. A seguir en el futuro porque el foco ahora es world c

--- new/Rana.h
+++ new/Rana.h
@@ -1,19 +1,55 @@
 #ifndef RANA_H
-
 #define RANA_H
+
+/*******************************************************************************
+ * HEADERS
+ ******************************************************************************/
+
 #include <stdint.h>
-#include "Objetos.h"
+#include "worldData.h"
+
+/*******************************************************************************
+ * CONSTANTES
+ ******************************************************************************/
+
 enum ARROW_KEYS {UP, DOWN, LEFT, RIGHT};
-#define POSX_INI ((int)WIDTH/2)
+#define POSX_INI (WIDTH/2)
 #define POSY_INI (HEIGHT-1)
 
+
+/*******************************************************************************
+ * ESTRUCTURAS Y TIPOS DE DATOS
+ ******************************************************************************/
+
 typedef struct{
-    uint8_t vidas;
+    uint8_t estado; //estado de actividad de la rana. 1 si puede ser controlada y 0 si ya llego al final y no puede ser controlada 
     int8_t posx;
     int8_t posy;
 } rana_t;
 
+/*******************************************************************************
+ * PROTOTIPOS DE FUNCIONES
+ ******************************************************************************/
+
+/**
+ * @brief Mueve la rana (prana) en la direccion pedidad (dir). Se ocupa tambien de llamar a ranaCollisions
+ * @param prana rana a mover
+ * @param dir direccion en la que moverse
+ * @param pl linea en la que se encuentra la rana
+*/
 void MoveRana(rana_t* prana, uint8_t dir, linea_t * pl);
-void RestarVidas(rana_t* pRana);
+
+/**
+ * @brief Se ocupa de que la rana interactue con el mundo. Tanto para morir si colisiona con algo fatal como para moverse si esta encima de algo como un tronco
+ * @param prana rana en cuestion
+ * @param pl linea en la que se encuentra la rana
+*/
+void RanaCollisions(rana_t * prana, linea_t * pl);
+
+/**
+ * @brief Resta una vida cuando es llamada. En caso de llegar a cero, se ocupa de accionar el game over.
+ * @param pRana se pide para poder reiniciar la posicion de la rana en caso de que no haya game over. 
+*/
+void RestarVidas(rana_t * pRana);
 
 #endif //RANA_H
--- old/ranaold.c
+++ old/ranaold.c
@@ -1,5 +1,5 @@
 #include "Rana.h"
-#include "worldData.h"
+#include "Objetos.h"
 
 extern int vidas;
 void MoveRana(rana_t* prana, uint8_t dir, linea_t * pl){
@@ -20,21 +20,19 @@ void MoveRana(rana_t* prana, uint8_t dir, linea_t * pl){
     }
 }
 
-void RanaCollisions(rana_t * prana, linea_t * pl){
-    if((prana->posx) > WIDTH || (prana->posx) < 0){ //Resta vidas si la rana se va por un costado del mapa
+void UpdateRanaData(rana_t * prana, linea_t * pl){
+    if((prana->posx) > 15 || (prana->posx) < 0){
         RestarVidas(prana);
     }
-
-    
+    *(((pl+(prana->posy))->plinea)+(prana->posx)) = 1;
 }
 
 void RestarVidas(rana_t* pRana){
-    //LLAMAR ANIMACION DE MUERTE CORRESPONDIENTE AL FRONTEND QUE SE ESTE USANDO
     vidas--;
     if(!vidas){
         //game over
     }
-    else{ //si quedan vidas, reinicia la posicion de la rana
+    else{
         pRana->posx = POSX_INI;
         pRana->posy = POSY_INI;
     }
