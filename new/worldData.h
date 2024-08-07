5f75ce359d6efeeed07a1408402d6fc535547217
jrodriguezarias <jrodriguezarias@itba.edu.ar>
2024-08-07 10:30:16 -0300

    worlddata.h

--- new/worldData.h
+++ new/worldData.h
@@ -1,11 +1,25 @@
 #ifndef WORLDDATA_H
 #define WORLDDATA_H
 
+/*******************************************************************************
+ * HEADERS
+ ******************************************************************************/
+
 #include <stdint.h>
+
+/*******************************************************************************
+ * CONSTANTES
+ ******************************************************************************/
+
 #define IZQ -1
 #define DER 1
 #define HEIGHT 16
 #define WIDTH 10
+#define MAX_OBJ 5
+
+/*******************************************************************************
+ * ESTRUCTURAS Y TIPOS DE DATOS
+ ******************************************************************************/
 
 typedef struct{
     uint8_t size; //ancho de los objetos en la linea
@@ -13,8 +27,12 @@ typedef struct{
     int8_t v; //velocidad de los objectos
     uint8_t val_def; //valor default de la linea. 0 es agua y 1 es tierra
     uint8_t cant_obj; //cantidad maxima de objetos que hay cargada en un momento 
-    int * p_linea; //puntero a coordenada (0, linea)
-    int * po; //arreglo que guarda posicion x de los objetos en la linea
+    int po[MAX_OBJ]; //arreglo que guarda posicion x de los objetos en la linea
+    uint8_t plinea[WIDTH]; //Arreglo
 } linea_t;
+
+/*******************************************************************************
+ * PROTOTIPOS DE FUNCIONES
+ ******************************************************************************/
 
 #endif //WORLDDATA_H
