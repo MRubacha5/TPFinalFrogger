bcd796cca1a2be325dd0c25e21fe7468087860d9
MRubacha5 <mrubacha@itba.edu.ar>
2024-11-29 10:41:31 -0300

    Update RaspiMainMenu.c

--- src/Frontend/RasPi/RaspiMainMenu.c
+++ src/Frontend/RasPi/RaspiMainMenu.c
@@ -1,31 +1,72 @@
 #include "RaspiData.h"
 
 
-void mainMenu(screenHandler_t * screenHandler, joystick_t * joystickHandler, rana_t * prana, worldData_t * worldData, linea_t * map){
+void menu(mainMenuHandler_t * mainMenuHandler, animationHandler_t * animationHandler, hiscoreHandler_t * hiscoreHandler, screenHandler_t * screenHandler, joystick_t * joystickHandler, rana_t * prana, worldData_t * worldData, linea_t * map){
     int i, c;
+
+    if(mainMenuHandler->screenSelected == 0){
+        for(i = 0 ; i < DISP_CANT_Y_DOTS ; i++){
+            for (c = 0 ; c < DISP_CANT_X_DOTS ; c++){
+                joystickHandler->pos.x = c;
+                joystickHandler->pos.y = i;
+                disp_write(joystickHandler->pos, mainMenu[i][c]);
+            }
+        }
+
+        for(i = (screenHandler->optionSelected?8:0) ; i < (screenHandler->optionSelected?16:8) ; i++){
+            for(c = 0 ; c < DISP_CANT_X_DOTS ; c++){
+                joystickHandler->pos.x = c;
+                joystickHandler->pos.y = i;
+                disp_write(joystickHandler->pos, !mainMenu[i][c]);
+            }
+        }
+    }
+    else{
+        for(i = 0 ; i < DISP_CANT_Y_DOTS ; i++){
+            for (c = 0 ; c < DISP_CANT_X_DOTS ; c++){
+                joystickHandler->pos.x = c;
+                joystickHandler->pos.y = i;
+                disp_write(joystickHandler->pos, mainMenu2[i][c]);
+            }
+        }
+
+        for(i = ((screenHandler->optionSelected-1)?8:0) ; i < ((screenHandler->optionSelected-1)?16:8) ; i++){
+            for(c = 0 ; c < DISP_CANT_X_DOTS ; c++){
+                joystickHandler->pos.x = c;
+                joystickHandler->pos.y = i;
+                disp_write(joystickHandler->pos, !mainMenu2[i][c]);
+            }
+        }
+    }
+
     
-    for(i = 0 ; i < DISP_CANT_Y_DOTS ; i++){
-        for (c = 0 ; c < DISP_CANT_X_DOTS ; c++){
-            joystickHandler->pos.x = c;
-            joystickHandler->pos.y = i;
-            disp_write(joystickHandler->pos, mainMenu[i][c]);
-        }
+
+
+    if(!(joystickHandler->joyMoved)){
+        joystickHandler->isMoving = 0;
     }
 
-    for(i = (screenHandler->optionSelected?8:0) ; i < (screenHandler->optionSelected?16:8) ; i++){
-        for(c = 0 ; c < DISP_CANT_X_DOTS ; c++){
-            joystickHandler->pos.x = c;
-            joystickHandler->pos.y = i;
-            disp_write(joystickHandler->pos, !mainMenu[i][c]);
-        }
-    }
+    if(joystickHandler->joyMoved && !(joystickHandler->isMoving)){
+        joystickHandler->isMoving = 1;
 
-    if(joystickHandler->joyMoved){
-        if(joystickHandler->joyValue == UP){
-            screenHandler->optionSelected = 0;
-        }
-        if(joystickHandler->joyValue == DOWN){
-            screenHandler->optionSelected = 1;
+        switch (joystickHandler->joyValue)
+        {
+        case DOWN:
+            if(screenHandler->optionSelected == 1){
+                mainMenuHandler->screenSelected = 1;
+            }
+            if(screenHandler->optionSelected == 0 || screenHandler->optionSelected == 1){
+                screenHandler->optionSelected++;
+            }
+            break;
+        case UP:
+            if(screenHandler->optionSelected == 1){
+                mainMenuHandler->screenSelected = 0;
+            }
+            if(screenHandler->optionSelected == 1 || screenHandler->optionSelected == 2){
+                screenHandler->optionSelected--;
+            }
+            break;
         }
     }
 
@@ -35,10 +76,13 @@ void mainMenu(screenHandler_t * screenHandler, joystick_t * joystickHandler, ran
             screenHandler->screen = GAME;
             gameStart(map, worldData);
             spawnRana(prana);
-            joystickHandler->timerCoord.x = 0;
-            joystickHandler->timerCoord.y = DISP_MAX_Y;
+            animationHandler->timerCoord.x = 0;
+            animationHandler->timerCoord.y = DISP_MAX_Y;
         }
         else if (screenHandler->optionSelected == 1){
+            hiscoreInit(screenHandler, hiscoreHandler);
+        }
+        else if (screenHandler->optionSelected == 2){
             screenHandler->do_exit = 1;
         }
     }
